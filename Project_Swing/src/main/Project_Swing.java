65f637c822ffe910d0cf70cad217e2daf19700b9
atafs <atafs@iscte-iul.pt>
2016-01-29 10:36:28 +0000

    update

---
+++ Project_Swing/src/main/Project_Swing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import swing.Gui_swing;
+
+/**
+ *
+ * @author Americo
+ */
+public class Project_Swing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gui_swing gui = new Gui_swing();
+        gui.setVisible(true);
+    }
+    
+}
